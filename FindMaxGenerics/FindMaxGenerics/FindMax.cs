d32a10e8eeff6a961cfd0d2fe5ba71f16f80ec03
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-21 15:21:44 +0530

    [Rohan]UC1: Find Max Int

---
+++ FindMaxGenerics/FindMaxGenerics/FindMax.cs
@@ -0,0 +1,16 @@
+﻿namespace FindMaxGenerics
+{
+    public class FindMax
+    {
+        public int FindMaximum(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                return firstValue;
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                return secondValue;
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+                return thirdValue;
+            return firstValue;
+        }
+    }
+}
---
+++ FindMaxGenerics/FindMaxGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FindMax intMax = new FindMax();
+            Console.WriteLine($"Maximum Int : {intMax.FindMaximum(40,50,60)}");
+            Console.ReadKey();
+        }
+    }
+}
