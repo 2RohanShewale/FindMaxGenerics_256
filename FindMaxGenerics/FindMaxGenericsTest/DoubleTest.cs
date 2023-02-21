893ebfe2c23c8953f61c2d29e5659e9caccf6cc5
Rohan Shewale <shewalerohan50@gmail.com>
2023-02-21 16:06:59 +0530

    [Rohan]UC2: TC2 max double at secong position

--- FindMaxGenerics/FindMaxGenericsTest/DoubleTest.cs
+++ FindMaxGenerics/FindMaxGenericsTest/DoubleTest.cs
@@ -11,5 +11,12 @@ namespace FindMaxGenericsTest
             Assert.AreEqual(15.1, result);
             Assert.Pass();
         }
+        [Test]
+        public void MaximumDouble_WhenSecondGivenValueIsMax()
+        {
+            double result = doubleMax.FindMaximum<double>(15.1, 17.03, 11.01);
+            Assert.AreEqual(17.03, result);
+            Assert.Pass();
+        }
     }
 }
